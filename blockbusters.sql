5056ba692dd3bfdfa94167b259a593da79e164f7
Sarah Thomas <sarahthomas.maria@gmail.com>
2022-02-28 10:42:18 -0500

    create movie table

--- blockbusters.sql
+++ blockbusters.sql
@@ -11,5 +11,19 @@ DROP TABLE IF EXISTS 'Transactions';
      'customer_id' int(11) NOT NULL,
      'customer_name' varchar(10) NOT NULL, 
      'customer_email' varchar(20) NOT NULL,
+     --Key
      PRIMARY KEY ('customer_id')
+ ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+ CREATE TABLE IF NOT EXISTS 'Movie'(
+     --Attributes
+     'movie_id' int(11) NOT NULL,
+     'movie_name' varchar(20) NOT NULL, 
+     'genre' varchar(10) NOT NULL,
+     'director_name' varchar(20) NOT NULL,
+     'rating' float(2,1),
+     'cast_list' varchar(200)
+     --Key
+     PRIMARY KEY ('movie_id')
  ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
