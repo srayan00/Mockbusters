f78fcca421877f3536c25ee44dbf431f8cc51ca9
Sahana <rayan.sahana@gmail.com>
2022-03-01 18:40:35 -0500

    Created the alchemy_init.py file and declared a mapping for Customer and Movie

--- Nupurs_task.py
+++ Nupurs_task.py
@@ -1,1 +1,19 @@
+from alchemy_init import SQL_ALCHEMY_DB as db
+
+
+class Customer(db.Model):
+    __tablename__ = "Customer"
+    customer_id = db.Column(db.Integer, primary_key = True, nullable= False)
+    customer_name = db.Column(db.String(20), nullable = False)
+    customer_email = db.Column(db.String(20), nullable = False)
+
+class Movie(db.Model):
+    __tablename__ = "Movie"
+    movie_id = db.Column(db.Integer, primary_key = True, nullable = False)
+    movie_name = db.Column(db.String(20), nullable = False)
+    genre = db.Column(db.String(10), nullable = False)
+    director_name = db.Column(db.String(20), nullable=False)
+    rating = db.Column(db.Float, nullable = False)
+    cast_list = db.Column(db.String(20), nullable = False)
+
 
