99ee1df129d1b85b9f738089f9253b1074f719ce
Nikita Aggarwal <aggarw64@purdue.edu>
2022-03-07 17:51:55 -0500

    added signup route

--- app.py
+++ app.py
@@ -1,36 +1,40 @@
-
-#instructions to run:
-   # in terminal:
-      # pip install virtualenv
-      # pip install flask
-      # export FLASK_APP=app
-      # export FLASK_ENV=development
-      # flask run (or "python -m flask run" if that doesn't work)
-   # go to filepath/login.html in the browser
+# instructions to run:
+# in terminal:
+# pip install virtualenv
+# pip install flask
+# export FLASK_APP=app
+# export FLASK_ENV=development
+# flask run (or "python -m flask run" if that doesn't work)
+# go to filepath/login.html in the browser
 
 # cnx = msql.connector.connect('mockbuster.db')
 
 from flask import Flask, redirect, url_for, request, render_template
+
 app = Flask(__name__)
 
-@app.route('/success/<name>')
-def success(name):
-   return 'welcome %s' % name
+
+# @app.route('/success/<name>')
+# def success(name):
+#    return 'welcome %s' % name
 
 @app.route('/', methods=['GET', 'POST'])
 def homepage():
-   return render_template('homepage.html')
+    return render_template('homepage.html')
 
-@app.route('/signup',methods = ['POST', 'GET'])
-def login():
-   if request.method == 'POST':
-      user = request.form['nm']
-      return redirect(url_for('success',name = user))
-   else:
-      user = request.args.get('nm')
-      return redirect(url_for('success',name = user))
+
+@app.route('/signup', methods=['POST', 'GET'])
+def signup():
+    return render_template('signup.html')
+
+
+# def login():
+#    if request.method == 'POST':
+#       user = request.form['nm']
+#       return redirect(url_for('success',name = user))
+#    else:
+#       user = request.args.get('nm')
+#       return redirect(url_for('success',name = user))
 
 if __name__ == '__main__':
-   app.run(debug = True)
-
-
+    app.run(debug=True)
