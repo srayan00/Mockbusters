4ac0e40272437b2f14861de1ac64b440137dbe19
Sahana <rayan.sahana@gmail.com>
2022-03-08 10:41:37 -0500

    Updated load_csv.py to delete rows from existing tables before loading. Messed up Mockbusters.db needs to be fixed. Made changes to .sql file to make it compatible with sqlite (remove ENGINE) and added the commented out files to a different .sql file

--- load_csv.py
+++ load_csv.py
@@ -1,5 +1,18 @@
 import sqlite3 as sql
+import pandas as pd
 
-
-con = sql.connect("Mockbusters.db")
+con = sql.connect("blockbusters.db")
 cur = con.cursor()
+tables = cur.fetchall()
+# for (table_name, ) in tables:
+#     print("1")
+#     print(table_name)
+
+cur.execute("""DELETE FROM `Catalog`;""")
+cur.execute("""DELETE FROM Transactions;""")
+cur.execute("""DELETE FROM Active_Rentals;""")
+cur.execute("""DELETE FROM Customer;""")
+cur.execute("""DELETE FROM Store;""")
+cur.execute("""DELETE FROM Movie;""")
+con.commit()
+
